54f641935f8fdd7ca9ae8ef53e23e96c1c27143d
Asier Cidon <acidonpe@redhat.com>
2020-10-19 14:43:17 +0200

    Deleted inncesary import

--- src/test/java/com/acidonper/myapp/web/UsersControllerTest.java
+++ src/test/java/com/acidonper/myapp/web/UsersControllerTest.java
@@ -7,7 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
